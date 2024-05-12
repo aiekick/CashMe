5b58ac739808a03d51bf4b85b63b74b059da1e65
aiekick <aiekick@funparadigm.com>
2024-05-13 01:13:04 +0200

    [WIP] : the LCL statement pdf import is working well. for the moment just debug print the table

--- plugins/LCLBroker/src/Headers/LCLBrokerBuild.h
+++ plugins/LCLBroker/src/Headers/LCLBrokerBuild.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #define LCLBroker_Prefix "LCLBroker"
-#define LCLBroker_BuildNumber 83
+#define LCLBroker_BuildNumber 115
 #define LCLBroker_MinorNumber 0
 #define LCLBroker_MajorNumber 0
-#define LCLBroker_BuildId "0.0.83"
+#define LCLBroker_BuildId "0.0.115"
--- plugins/LCLBroker/src/Modules/PdfAccountStatementModule.cpp
+++ plugins/LCLBroker/src/Modules/PdfAccountStatementModule.cpp
@@ -5,6 +5,7 @@
 
 #include <memory>
 #include <vector>
+#include <array>
 #include <map>
 
 #include <xpdf/goo/gmem.h>
@@ -44,18 +45,18 @@ struct Token {
     }
 };
 
-typedef int32_t PageIdx, PosX, PosY;
-struct Field {
-    PosX px;
-    PosY py;
-    std::string field;
+typedef int32_t PageIdx, PosX, PosY, SizeX, SizeY;
+struct Fields {
+    bool isHeader = false;
+    std::vector<Token> fields;
+    std::vector<double> hxs;  // the x of each header column
 };
-typedef std::vector<Field> Fields;
+
 typedef std::vector<Fields> Table;
+typedef std::map<PageIdx, std::map<PosY, std::map<PosX, Token>>> TokenContainer;
 
 class TableExtractor : public OutputDev {
 private:
-    typedef std::map<PageIdx, std::map<PosY, std::map<PosX, std::string>>> TokenContainer;
     TokenContainer m_Tokens;
     bool m_IsFirstTokenChar = false;
     Token m_TempToken;
@@ -88,10 +89,6 @@ public:
         return true;
     }
 
-    void startPage(int pageNum, GfxState *state) override {
-      //  LogVarDebugInfo("startPage(%i)", pageNum);
-    }
-
     void endPage() override {
       //  LogVarDebugInfo("endPage()");
         ++m_CurrentPageIdx;
@@ -103,9 +100,13 @@ public:
     }
 
     void endString(GfxState *state) override {
-        int32_t ipy = std::abs((int32_t)m_TempToken.py);
-        int32_t ipx = std::abs((int32_t)m_TempToken.px);
-        m_Tokens[m_CurrentPageIdx][ipy][ipx] = m_TempToken.token;
+        m_TempToken.px = std::abs(m_TempToken.px);
+        m_TempToken.py = std::abs(m_TempToken.py);
+        m_TempToken.sx = std::abs(m_TempToken.sx);
+        m_TempToken.sy = std::abs(m_TempToken.sy);
+        int32_t ipy = static_cast<int32_t>(m_TempToken.py);
+        int32_t ipx = static_cast<int32_t>(m_TempToken.px);
+        m_Tokens[m_CurrentPageIdx][ipy][ipx] = m_TempToken;
         //m_TempToken.print();
     }
 
@@ -133,61 +134,171 @@ public:
         m_TempToken.token += c;
     }
 
-    void drawString(GfxState *state, GString *s, GBool fill, GBool stroke, GBool makePath) override {
-       // LogVarDebugInfo("drawString(\"%s\")", s->getCString());
-    }
-
-    void incCharCount(int nChars) override {
-      //  LogVarDebugInfo("incCharCount(%i)", nChars);
-    }
-
-    void beginActualText(GfxState *state, Unicode *u, int uLen) override {
-     //   LogVarDebugInfo("beginActualText(%u,%i,%i)", (uintptr_t)state, (u != nullptr ? *u : -1), uLen);
-    }
-
-    void endActualText(GfxState *state) override {
-     //   LogVarDebugInfo("endActualText(%u)", (uintptr_t)state);
-    }
-
-    void stroke(GfxState *state) override  {
-        GfxPath *path;
-        GfxSubpath *subpath;
-        double x[2], y[2], t;
-        path = state->getPath();
-        if (path->getNumSubpaths() != 1) {
-            return;
-        }
-        subpath = path->getSubpath(0);
-        if (subpath->getNumPoints() != 2) {
-            return;
-        }
-        state->transform(subpath->getX(0), subpath->getY(0), &x[0], &y[0]);
-        state->transform(subpath->getX(1), subpath->getY(1), &x[1], &y[1]);
-        // look for a vertical or horizontal line
-        if (x[0] == x[1] || y[0] == y[1]) {
-            if (x[0] > x[1]) {
-                t = x[0];
-                x[0] = x[1];
-                x[1] = t;
-            }
-            if (y[0] > y[1]) {
-                t = y[0];
-                y[0] = y[1];
-                y[1] = t;
-            }
-            if (x[0] == x[1]) {
-                LogVarDebugInfo("stroke Vertical : %f,%f,%f,%f", x[0], y[0], x[1], y[1]);
-            }
-            if (y[0] == y[1]) {
-                LogVarDebugInfo("stroke Horizontal : %f,%f,%f,%f", x[0], y[0], x[1], y[1]);
-            }
-        } else {
-        
-        }
-        LogVarDebugInfo("stroke : %f,%f,%f,%f", x[0], y[0], x[1], y[1]);
-    }
+public:
 };
 
+class TableSolver {
+private:
+    typedef std::array<Token, 5> StatementFields;
+    typedef std::array<int32_t, 5> ColumnSizes;
+    typedef std::vector<StatementFields> StatementRows;
+    ColumnSizes m_ColSizes = {};
+
+public:
+    void compute(const TokenContainer &vContainer) {
+        const auto &tbl = fillTable(vContainer);
+        const auto &stms = solveTableColumns(tbl);
+        printTable(stms);
+    }
+
+private:
+    Table fillTable(const TokenContainer &vContainer) {
+        Table ret;
+        Fields fields;
+        bool tabled_started = false;
+        bool doc_end = false;
+        bool can_save_row = false;
+        const auto &pages_count = (int32_t)vContainer.size();
+        for (const auto &page : vContainer) {
+            const auto &page_idx = page.first;
+            const auto &page_footer = ct::toStr("  Page %u / %u", page_idx + 1, pages_count);
+            for (const auto &row : page.second) {
+                const auto &row_idx = row.first;
+                const auto &cols = row.second;
+                fields = {};
+                for (const auto &col : cols) {
+                    fields.fields.push_back(col.second);
+                }
+                can_save_row = true;
+                if (fields.fields.size() == 5U) {
+                    if (fields.fields.at(0).token == "DATE" &&     //
+                        fields.fields.at(1).token == "LIBELLE" &&  //
+                        fields.fields.at(2).token == "VALEUR" &&   //
+                        fields.fields.at(3).token == "DEBIT" &&    //
+                        fields.fields.at(4).token == "CREDIT") {
+                        fields.isHeader = true;
+                        tabled_started = true;  // start of a table
+                        fields.hxs.push_back(0.0);
+                        for (const auto &f : fields.fields) {
+                            fields.hxs.push_back(f.px + f.sx);  // x coord of column separator
+                        }
+                    }
+                } else if (fields.fields.size() == 1U) {
+                    if (tabled_started) {
+                        if (fields.fields.at(0).token == page_footer) {
+                            tabled_started = false;  // end of a table
+                        }
+                    }
+                } else if (fields.fields.size() == 2U) {
+                    if (tabled_started) {
+                        // SOLDE INTERMEDIAIRE ou SOLDE EN EUROS pour la fin
+                        // du doc en evitant les tables de fin de fichiers
+                        if (fields.fields.at(0).token.find("SOLDE INTERMEDIAIRE") != std::string::npos) {
+                            tabled_started = false;  // end of a table
+                            return ret;
+                        }
+                    }
+                } else if (fields.fields.size() == 3U) {
+                    if (tabled_started) {
+                        // ANCIEN SOLDE => on saute la ligne
+                        if (fields.fields.at(1).token.find("ANCIEN SOLDE") != std::string::npos) {
+                            can_save_row = false;  // end of a table
+                        }
+                    }
+                }
+                if (can_save_row && tabled_started && !doc_end) {
+                    ret.push_back(fields);
+                }
+            }
+        }
+        return ret;
+    }
+    StatementRows solveTableColumns(const Table &vTable) {
+        StatementRows ret;
+        if (!vTable.empty()) {
+            bool headerAlreadyAdded = false;
+            Fields currentHeader;
+            for (const auto &row : vTable) {
+                if (row.isHeader) {
+                    currentHeader = row;
+                } 
+                StatementFields f;
+                for (const auto &col : row.fields) {
+                    // we need to find the column
+                    // from end to styart, for find the last column
+                    int32_t c = currentHeader.hxs.size() - 1;
+                    for (; c >= 0; --c) {
+                        if (col.px > currentHeader.hxs.at(c)) {
+                            break;
+                        }
+                    }
+                    if (c < 0) {
+                        CTOOL_DEBUG_BREAK;
+                    }
+                    if (c < m_ColSizes.size()) {
+                        f[c] = col;
+                        // for table printing
+                        if (col.token.size() > m_ColSizes.at(c)) {
+                            if (col.token.size() > 100) {
+                                CTOOL_DEBUG_BREAK;
+                            }
+                            m_ColSizes[c] = col.token.size();  // the sx is the new max width of the column
+                        }
+                    }
+                }
+                if (row.isHeader) { // add only one header
+                    if (!headerAlreadyAdded) {
+                        ret.push_back(f);
+                        headerAlreadyAdded = true;
+                    }
+                } else {
+                    ret.push_back(f);
+                }
+            }
+        }
+        return ret;
+    }
+    void printTable(const StatementRows &vStms) {
+        bool isHeader = true;
+        std::string h_line;
+        std::string row_str;
+        bool is_new_line = false;
+        bool is_last_line_the_header = true;
+        for (const auto &row : vStms) {
+            row_str.clear();
+            is_new_line = false;
+            for (size_t idx = 0; idx < 5; ++idx) {
+                const auto &tk = row.at(idx);
+                const auto &colSize = m_ColSizes.at(idx);
+                const auto w = colSize - tk.token.size();
+                if (w < 0) {
+                    CTOOL_DEBUG_BREAK;
+                }
+                if (idx == 0) {
+                    if (!tk.token.empty()) {
+                        is_new_line = true;
+                    }
+                }
+                row_str += " | " + tk.token + std::string(w, ' ');
+            }
+            row_str += " | ";
+            if (isHeader) {
+                h_line = std::string(row_str.size(), '=');
+                LogVarDebugInfo("%s", h_line.c_str());
+            } else if (is_new_line && !is_last_line_the_header) {
+                LogVarDebugInfo("%s", std::string(row_str.size(), '-').c_str());
+            }        
+            LogVarDebugInfo("%s", row_str.c_str());
+            if (isHeader) {
+                LogVarDebugInfo("%s", h_line.c_str());
+                isHeader = false;
+            } else {
+                is_last_line_the_header = false;
+            }
+        }
+        LogVarDebugInfo("%s", h_line.c_str());
+    }
+};
 
 Cash::BankStatementModulePtr PdfAccountStatementModule::create() {
     auto res = std::make_shared<PdfAccountStatementModule>();
@@ -201,7 +312,7 @@ Cash::AccountStatements PdfAccountStatementModule::importBankStatement(const std
     Cash::AccountStatements ret{};
     auto ps = FileHelper::Instance()->ParsePathFileName(vFilePathName);
     if (ps.isOk) {
-        auto pdf_doc_ptr = new PDFDoc(new GString(vFilePathName.c_str()), nullptr, nullptr); // the GString is delete by PDFDoc
+        auto pdf_doc_ptr = new PDFDoc(new GString(vFilePathName.c_str()), nullptr, nullptr); // the GString is deleted by PDFDoc
         if (pdf_doc_ptr != nullptr) {
             if (!pdf_doc_ptr->isOk()) {
                 LogVarError("Fail, cant read the pdf %s", vFilePathName.c_str());
@@ -210,65 +321,17 @@ Cash::AccountStatements PdfAccountStatementModule::importBankStatement(const std
                 if (pages_count == 0) {
                     LogVarError("Fail, no pages are available in the pdf %s", vFilePathName.c_str());
                 } else {
+                    globalParams = new GlobalParams(nullptr);  // globalParams ios a extern and is used by pdf_doc_ptr...
+
+                    LogVarDebugInfo("LCl Table Extraction for %s", vFilePathName.c_str());
+
                     TableExtractor tbl;
-                    globalParams = new GlobalParams(nullptr); // globalParams ios a extern and is used by pdf_doc_ptr...
                     pdf_doc_ptr->displayPages(&tbl, 1, pages_count, 72, 72, 0, gFalse, gTrue, gFalse);
+
+                    TableSolver ts;
+                    ts.compute(tbl.getTokenContainer());
+
                     delete globalParams;
-
-                    // fill Table
-                    Table _table;
-                    {
-                        Fields fields;
-                        Field f;
-                        bool tabled_started = false;
-                        const auto &tokens = tbl.getTokenContainer();
-                        pages_count = (int32_t)tokens.size();
-                        for (const auto &page : tokens) {
-                            const auto &page_idx = page.first;
-                            const auto &page_footer = ct::toStr("  Page %u / %u", page_idx + 1, pages_count);
-                            for (const auto &row : page.second) {
-                                const auto &row_idx = row.first;
-                                const auto &cols = row.second;
-                                fields = {};
-                                for (const auto &col : cols) {
-                                    f = {};
-                                    f.px = col.first;
-                                    f.py = row.first;
-                                    f.field = col.second;
-                                    fields.push_back(f);
-                                }
-                                if (fields.size() == 5U) {
-                                    if (fields.at(0).field == "DATE" &&     //
-                                        fields.at(1).field == "LIBELLE" &&  //
-                                        fields.at(2).field == "VALEUR" &&   //
-                                        fields.at(3).field == "DEBIT" &&    //
-                                        fields.at(4).field == "CREDIT") {
-                                        tabled_started = true;
-                                    }
-                                } else if (fields.size() == 1U) {
-                                    if (tabled_started) {
-                                        if (fields.at(0).field == page_footer) {
-                                            tabled_started = false;
-                                        }
-                                    }
-                                }
-                                if (tabled_started) {
-                                    _table.push_back(fields);
-                                }
-                            }
-                        }
-                    }
-
-                    // print table
-                    {
-                        for (const auto &row : _table) {
-                            std::string row_str;
-                            for (const auto &col : row) {
-                                row_str += " \t\"" + col.field + "\"";
-                            }
-                            LogVarDebugInfo("%s", row_str.c_str());
-                        }
-                    }
                 }
             }
         }
