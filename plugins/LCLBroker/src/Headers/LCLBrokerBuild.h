7fba430d794fe585f377eb3c9b179f9f566d25f2
aiekick <aiekick@funparadigm.com>
2024-12-07 21:08:52 +0100

    -

--- plugins/LCLBroker/src/Headers/LCLBrokerBuild.h
+++ plugins/LCLBroker/src/Headers/LCLBrokerBuild.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #define LCLBroker_Prefix "LCLBroker"
-#define LCLBroker_BuildNumber 305
+#define LCLBroker_BuildNumber 308
 #define LCLBroker_MinorNumber 0
 #define LCLBroker_MajorNumber 0
-#define LCLBroker_BuildId "0.0.305"
+#define LCLBroker_BuildId "0.0.308"
